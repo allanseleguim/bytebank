7b2a7ca73330e000e14a11e492fbd0d095fa5820
allanseleguim <allan@agenciawebsorocaba.com.br>
2021-07-28 18:13:33 -0300

    Front-end funcional com API JSON SERVER

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,45 @@
+import { Router } from '@angular/router';
+import { TransferenciaService } from './../services/transferencia.service';
+import { Transferencia } from './../models/transferencia.model';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+      selector: 'app-nova-transferencia',
+      templateUrl: './nova-transferencia.component.html',
+      styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+      @Output() aoTransferir = new EventEmitter<any>();
+
+      valor: number;
+      destino: number;
+
+      constructor(
+            private service: TransferenciaService,
+            private router: Router
+      ) { }
+
+      transferir() {
+            console.log('Solicitada nova transferência');
+            const valorEmitir: Transferencia = {
+                  valor: this.valor,
+                  destino: this.destino
+            };
+            //this.aoTransferir.emit(valorEmitir);
+            this.service.adicionar(valorEmitir).subscribe(resultado => {
+                  console.log(resultado);
+                  this.router.navigateByUrl('extrato');
+                  this.limparCampos();
+            },
+                  error => console.error(error)
+            );
+
+
+      }
+
+      limparCampos() {
+            this.valor = 0;
+            this.destino = 0;
+      }
+}
